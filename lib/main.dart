b2e23c3da39ce0e2e94b6effc7625d4b28589211
Manan Karnik <manankarnik03@gmail.com>
2023-01-21 22:20:36 +0530

    build: add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import "package:flutter/material.dart";
 import "registration_page.dart";
 import "login_page.dart";
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
