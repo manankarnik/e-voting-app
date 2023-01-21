568509a5c69d663033e94c1cb2a9beb42dc48b77
Manan Karnik <manankarnik03@gmail.com>
2023-01-21 16:56:48 +0530

    refactor: move login page to seperate file

---
+++ lib/login_page.dart
@@ -0,0 +1,82 @@
+import "package:flutter/material.dart";
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(child: Text("E-Voting")),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: const [
+          Text(
+            "Login",
+            style: TextStyle(
+              fontSize: 30,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(24),
+            child: LoginForm(),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class LoginForm extends StatefulWidget {
+  const LoginForm({super.key});
+
+  @override
+  State<LoginForm> createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  final _formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          TextFormField(
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: "Enter your mobile number",
+            ),
+            validator: (value) {
+              if (value == null || value.length != 10) {
+                return "Please enter valid mobile number";
+              }
+              return null;
+            },
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              minimumSize: const Size.fromHeight(50),
+            ),
+            onPressed: () {
+              if (_formKey.currentState!.validate()) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text("Processing Data")),
+                );
+              }
+            },
+            child: const Text(
+              "Generate OTP",
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
+import "login_page.dart";
 
 void main() {
   runApp(const MyApp());
@@ -17,86 +18,5 @@ class MyApp extends StatelessWidget {
         ),
         debugShowCheckedModeBanner: false,
         home: const LoginPage());
-  }
-}
-
-class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("E-Voting"),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: const [
-          Text(
-            "Login",
-            style: TextStyle(
-              fontSize: 30,
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(24),
-            child: LoginForm(),
-          )
-        ],
-      ),
-    );
-  }
-}
-
-class LoginForm extends StatefulWidget {
-  const LoginForm({super.key});
-
-  @override
-  State<LoginForm> createState() => _LoginFormState();
-}
-
-class _LoginFormState extends State<LoginForm> {
-  final _formKey = GlobalKey<FormState>();
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        children: [
-          TextFormField(
-            decoration: const InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: "Enter your mobile number",
-            ),
-            validator: (value) {
-              if (value == null || value.length != 10) {
-                return 'Please enter valid mobile number';
-              }
-              return null;
-            },
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              minimumSize: const Size.fromHeight(50),
-            ),
-            onPressed: () {
-              if (_formKey.currentState!.validate()) {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(content: Text('Processing Data')),
-                );
-              }
-            },
-            child: const Text(
-              "Generate OTP",
-              style: TextStyle(fontSize: 20),
-            ),
-          ),
-        ],
-      ),
-    );
   }
 }
