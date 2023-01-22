c5f90a8a52e87a963037944c4a39d29f7434ad44
Manan Karnik <manankarnik03@gmail.com>
2023-01-22 23:51:19 +0530

    refactor: remove unused import

--- lib/auth.dart
+++ lib/auth.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'otp_validation.dart';
-import 'login_page.dart';
 
 FirebaseAuth auth = FirebaseAuth.instance;
 
