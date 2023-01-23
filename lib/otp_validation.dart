4609642ec019b71023e1cc06ce7c8ef6c4abd037
Manan Karnik <manankarnik03@gmail.com>
2023-01-23 22:47:11 +0530

    feat: handle invalid otp exception

--- lib/otp_validation.dart
+++ lib/otp_validation.dart
@@ -5,15 +5,22 @@ import 'home_page.dart';
 
 FirebaseAuth auth = FirebaseAuth.instance;
 
-class OtpValidation extends StatelessWidget {
-  OtpValidation(this.phoneNumber, this.verificationId, this.resendToken,
+class OtpValidation extends StatefulWidget {
+  const OtpValidation(this.phoneNumber, this.verificationId, this.resendToken,
       {super.key});
 
   final String verificationId;
   final String phoneNumber;
   final int? resendToken;
 
+  @override
+  State<OtpValidation> createState() => _OtpValidationState();
+}
+
+class _OtpValidationState extends State<OtpValidation> {
   final TextEditingController otpController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  bool isValid = false;
 
   @override
   Widget build(BuildContext context) {
@@ -36,7 +43,36 @@ class OtpValidation extends StatelessWidget {
               const SizedBox(
                 height: 20,
               ),
-              PinInputTextField(controller: otpController),
+              Form(
+                key: _formKey,
+                child: PinInputTextFormField(
+                    onChanged: (text) async {
+                      if (text.length == 6) {
+                        String otp = otpController.text;
+                        // Create a PhoneAuthCredential with the code
+                        PhoneAuthCredential credential =
+                            PhoneAuthProvider.credential(
+                                verificationId: widget.verificationId,
+                                smsCode: otp);
+
+                        // Sign the user in (or link) with the credential
+                        await auth
+                            .signInWithCredential(credential)
+                            .then((value) => setState(() => isValid = true))
+                            .catchError(
+                          (e) {
+                            isValid = false;
+                            print(e);
+                          },
+                        );
+                      }
+                    },
+                    validator: (value) {
+                      if (isValid) return null;
+                      return "Invalid OTP";
+                    },
+                    controller: otpController),
+              ),
               const SizedBox(
                 height: 20,
               ),
@@ -45,21 +81,14 @@ class OtpValidation extends StatelessWidget {
                     minimumSize: const Size.fromHeight(50),
                   ),
                   onPressed: () async {
-                    String otp = otpController.text;
-                    // Create a PhoneAuthCredential with the code
-                    PhoneAuthCredential credential =
-                        PhoneAuthProvider.credential(
-                            verificationId: verificationId, smsCode: otp);
-
-                    // Sign the user in (or link) with the credential
-                    await auth.signInWithCredential(credential).then(
-                          (value) => Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => HomePage(phoneNumber),
-                            ),
-                          ),
-                        );
+                    if (_formKey.currentState!.validate()) {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => HomePage(widget.phoneNumber),
+                        ),
+                      );
+                    }
                   },
                   child: const Text('Submit'))
             ]),
